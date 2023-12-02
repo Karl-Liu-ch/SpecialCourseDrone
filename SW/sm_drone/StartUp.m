02757755b0383a7bca18d5b320ea59f3d1da3db1
Karl-Liu-ch <liu_ht2010@qq.com>
2023-12-02 20:46:02 +0100

    add aerodynamic propellers

--- SW/sm_drone/StartUp.m
+++ SW/sm_drone/StartUp.m
@@ -1,10 +1,11 @@
 x_0 = 0;
 y_0 = 0;
 z_0 = -0.01;
-Gain_p = 12;
+Gain_p = 0;
 
 mass_motor = 0.05;
 mass_prop = 0.03;
 battery_mass = 0.5;
 platform_mass = battery_mass + 0.1;
 servo_mass = 0.06;
+coaxialeff = 0.8;
