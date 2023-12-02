bcdde5a6cb15fd6a510728b04ccf0b5a857be94b
Karl-Liu-ch <liu_ht2010@qq.com>
2023-12-02 18:08:29 +0100

    update

--- SW/sm_drone/StartUp.m
+++ SW/sm_drone/StartUp.m
@@ -1,4 +1,10 @@
 x_0 = 0;
 y_0 = 0;
-z_0 = 1;
-Gain_p = 3;
+z_0 = -0.01;
+Gain_p = 12;
+
+mass_motor = 0.05;
+mass_prop = 0.03;
+battery_mass = 0.5;
+platform_mass = battery_mass + 0.1;
+servo_mass = 0.06;
